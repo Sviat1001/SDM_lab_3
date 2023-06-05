35914d13d16ed4e7f5c4b52c95258ab115f13253
Sviat Oleksyn <sviat1001@gmail.com>
2023-06-05 16:40:16 +0300

    Changed api.py

--- lab-03-starter-project-python-main/spaceship/routers/api.py
+++ lab-03-starter-project-python-main/spaceship/routers/api.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter
 
+import numpy as numpy
+
 router = APIRouter()
 
 
@@ -8,4 +10,18 @@ def hello_world() -> dict:
     return {
         'msg': 'Hello, World!',
         'name': 'Sviat',
-        'surname': 'Oleksyn'}
+        'surname': 'Oleksyn'
+    }
+
+@router.get('/matrix')
+def matrix() -> dict:
+    matrix_a = numpy.random.rand(10, 10)
+    matrix_b = numpy.random.rand(10, 10)
+
+    product = numpy.dot(matrix_a, matrix_b)
+
+    return {
+        "matrix A": matrix_a.tolist(),
+        "matrix B": matrix_b.tolist(),
+        "result": product.tolist()
+    }
